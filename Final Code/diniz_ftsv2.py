64d855b44f3b26a00983471ad44b90a8bfd1a45b
apaulinhamd <49566742+apaulinhamd@users.noreply.github.com>
2024-01-07 21:20:28 -0300

    Update diniz_ftsv2.py

--- Final Code/diniz_ftsv2.py
+++ Final Code/diniz_ftsv2.py
@@ -76,8 +76,9 @@ def split_datasets(r, X, delay, n_features):
     N_tst_maj = int(0.9334 * N_tst) # 93,34% das amostras = normal
     N_tst_min = int(0.0666 * N_tst) # 6,66% das amostras = clogging
 
+    # Atualiza o índice de início para o próximo fold
     #start_idx = r * (N_tr + N_val + N_tst)
-    start_idx = r * (N_tst_maj + N_val_maj + N_tr_maj + 3)
+    start_idx = (r+1) * (N_tst_maj + N_val_maj + N_tr_maj + 3)
 
     # Amostras de treinamento
     end_train_maj = start_idx + N_tr_maj
