cac52b0f625600c4574dd9d23e2eae419ba07cb3
apaulinhamd <49566742+apaulinhamd@users.noreply.github.com>
2024-01-08 08:53:09 -0300

    Update diniz_ftsv2.py

--- Final Code/diniz_ftsv2.py
+++ Final Code/diniz_ftsv2.py
@@ -185,8 +185,8 @@ def split_dataset(X, delay, n_features):
     # concatenate the validation and test dataset
     X_valid = np.concatenate([X_val_0, X_val_1])
     X_test = np.concatenate([X_tst_0, X_tst_1])   
-    np.random.shuffle(X_valid)
-    np.random.shuffle(X_test)
+    #np.random.shuffle(X_valid)
+    #np.random.shuffle(X_test)
 	
 
     # defines input and output of validation and test datasets
